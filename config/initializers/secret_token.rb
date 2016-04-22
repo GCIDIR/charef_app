5295e402d86df13c1f30489a4775ab7a8f5d468a
Idir GACI <idir.gaci@gmail.com>
2016-04-22 21:24:31 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharefApp::Application.config.secret_token = 'befc71bdef1e6371db9449d4d82e3faecef0bcbe3accb35a857e1aedc4ea19e5184b0003eaccfd1715f37a668caded346fae21f22409377ebd607d44fd7e2530'
